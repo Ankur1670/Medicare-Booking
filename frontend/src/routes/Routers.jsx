250f887c8ff45d6e729eb5b423d9c63682035d68
Ankur1670 <16sainiankur@gmail.com>
2024-08-22 13:36:52 +0530

    newcommit

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -5,8 +5,11 @@ import Signup from '../pages/Signup'
 import Contact from '../pages/Contact'
 import Doctors from '../pages/Doctors/Doctors'
 import DoctorDetails from '../pages/Doctors/DoctorDetails'
+import MyAccount from '../Dashboard/user-account/MyAccount'
 
 import { Routes , Route } from 'react-router-dom'
+import Dashboard from '../Dashboard/doctor-account/Dashboard'
+import ProtectedRoute from './ProtectedRoute'
 const Routers = () => {
   return (
    <Routes>
@@ -18,6 +21,10 @@ const Routers = () => {
     <Route path='/register' element={<Signup/>}/>
     <Route path='/contact' element={<Contact/>}/>
     <Route path='/services' element={<Services/>}/>
+    <Route path='/user/profile/me' element={<ProtectedRoute allowedRoles={['patient']}><MyAccount/></ProtectedRoute>}/>
+    <Route path='/doctors/profile/me' element={<ProtectedRoute allowedRoles={['doctor']}><Dashboard/></ProtectedRoute>}/>
+
+
 
    
 
